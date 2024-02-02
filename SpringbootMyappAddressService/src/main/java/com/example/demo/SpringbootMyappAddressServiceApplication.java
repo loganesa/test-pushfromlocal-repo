3d1936f7a3bb19aa1a5be327f630bd155e790da4
loganesa <157871344+loganesa@users.noreply.github.com>
2024-02-01 20:07:36 -0600

    Update SpringbootMyappAddressServiceApplication.java
    commit from github

--- SpringbootMyappAddressService/src/main/java/com/example/demo/SpringbootMyappAddressServiceApplication.java
+++ SpringbootMyappAddressService/src/main/java/com/example/demo/SpringbootMyappAddressServiceApplication.java
@@ -8,7 +8,7 @@ public class SpringbootMyappAddressServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMyappAddressServiceApplication.class, args);
-		System.out.println("this is address service");
+		System.out.println("this is address service from git");
 	}
 
 }
