309735ddfb55686feac7365d8658abee84d84e62
logan <logan@LoguIndia.lan>
2024-02-01 19:31:04 -0600

    initial commit

--- SpringbootMyappAddressService/src/main/java/com/example/demo/SpringbootMyappAddressServiceApplication.java
+++ SpringbootMyappAddressService/src/main/java/com/example/demo/SpringbootMyappAddressServiceApplication.java
@@ -8,6 +8,7 @@ public class SpringbootMyappAddressServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMyappAddressServiceApplication.class, args);
+		System.out.println("this is address service");
 	}
 
 }
